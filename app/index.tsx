8dc2c1f8bf8434bf2088a12c0bdda3a9abd0fb75
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2025-04-29 15:43:39 -0300

    Remove default tab navigation

--- app/index.tsx
+++ app/index.tsx
@@ -23,9 +23,8 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
+          Edit <ThemedText type="defaultSemiBold">app/index.tsx</ThemedText> to
+          see changes. Press{" "}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: "cmd + d",
